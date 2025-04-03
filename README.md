607276fd46dffdf20b4ba2bbb6d9074ce1c891f4
Samuel <sam404kim@gmail.com>
2025-04-03 09:38:15 -0700

    changed model

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
   1. Start up a server 
 
      - uvicorn main:app --reload
+
+  2. Update requirement.txt
+
+     - pip freeze > requirements.txt
--- main.py
+++ main.py
@@ -14,11 +14,10 @@ import os
 
 
 # set device to gpu
-gpu_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+targeted_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-weights = torchvision.models.ResNet50_Weights.DEFAULT # .DEFAULT = best available weights 
-model_0 = torchvision.models.resnet50(weights=weights).to(gpu_device)
+weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT # .DEFAULT = best available weights 
+model_0 = torchvision.models.efficientnet_b0(weights=weights).to(targeted_device)
 
 
 app = FastAPI()
@@ -55,7 +54,7 @@ response.raise_for_status()  # Optional: will throw error if download fails
 buffer = BytesIO(response.content)
 
 # Load model
-model_0.load_state_dict(torch.load(buffer, map_location=torch.device(gpu_device)))
+model_0.load_state_dict(torch.load(buffer, map_location=torch.device(targeted_device)))
 model_0.eval()
 
 # Image transform
@@ -67,6 +66,20 @@ transform = transforms.Compose([
 
 with open('name of the animals.txt', 'r') as f:
     labels = [line.strip() for line in f if line.strip()]
+
+
+import threading
+import time
+import psutil
+
+def log_memory_usage():
+    while True:
+        mem = psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2
+        print(f"[MEMORY] {mem:.2f} MB")
+        time.sleep(10)
+
+threading.Thread(target=log_memory_usage, daemon=True).start()
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
